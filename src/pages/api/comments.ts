d549c304cfd9bbaf991695c541a77ba7a6250339
卫星w <1731164346@qq.com>
2025-12-09 17:20:37 +0800

    Set runtime to edge for comments API
    export const runtime = 'edge';  Cloudflare Pages 要求 Next.js 中所有的动态API路由（像 /api/xxx）都必须明确声明为边缘运行时，否则无法部署。

--- src/pages/api/comments.ts
+++ src/pages/api/comments.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/lib/prisma";
-
+export const runtime = 'edge';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
