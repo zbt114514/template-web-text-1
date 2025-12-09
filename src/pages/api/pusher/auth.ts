ab1c4ebf0e9ee096d4321173ac3e6672df8ead64
卫星w <1731164346@qq.com>
2025-12-09 17:21:55 +0800

    Set runtime to edge for Pusher auth API
    export const runtime = 'edge';  Cloudflare Pages 要求 Next.js 中所有的动态API路由（像 /api/xxx）都必须明确声明为边缘运行时，否则无法部署。

--- src/pages/api/pusher/auth.ts
+++ src/pages/api/pusher/auth.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import pusher from "@/lib/pusher";
-
+export const runtime = 'edge';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
