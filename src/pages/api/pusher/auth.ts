d1da35c255234962b21f2f05d982ac6efe14d250
卫星w <1731164346@qq.com>
2025-12-09 17:39:52 +0800

    Change runtime from edge to nodejs for Pusher auth

--- src/pages/api/pusher/auth.ts
+++ src/pages/api/pusher/auth.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import pusher from "@/lib/pusher";
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
