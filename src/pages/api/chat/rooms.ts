4ef547bc37171ed5a534160292148363fe0356c2
卫星w <1731164346@qq.com>
2025-12-09 17:38:25 +0800

    Change runtime from 'edge' to 'nodejs'

--- src/pages/api/chat/rooms.ts
+++ src/pages/api/chat/rooms.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/lib/prisma";
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
