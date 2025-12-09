ccffee31b350077fa5e143620c035913006f92c0
卫星w <1731164346@qq.com>
2025-12-09 18:17:54 +0800

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,28 @@
 // open-next.config.ts
-export 默认 {
-  default: {}
+import 输入 { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  default: {
+    minify: true,
+    buildCommand: "npm run build",
+    routes: [
+      {
+        src: "/api/.*",
+        dest: "/api/[[path]]",
+        continue: false
+      },
+      {
+        src: "/_next/static/.*",
+        dest: "/_next/static/[[path]]",
+        continue: false
+      },
+      {
+        src: "/(.*)",
+        dest: "/[[path]]",
+        continue: false
+      }
+    ]
+  }
 };
+
+export default config;
